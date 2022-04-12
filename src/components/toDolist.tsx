5344afe6b1d305599790f4d6239fd29be80f39a2
jisooyoon <ygs12567@gmail.com>
2022-04-12 13:50:26 +0900

    Use useRef in react.js with typescript

--- src/components/toDolist.tsx
+++ src/components/toDolist.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const TodoList = () => {
-  return null;
+  const [value, setValue] = useState<string>();
+  // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+  //   const {
+  //     currentTarget: { value },
+  //   } = event;
+  //   setValue(value);
+  // };
+  const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setValue(inputRef.current.value);
+    console.log(inputRef.current.value);
+    inputRef.current.value = '';
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" ref={inputRef} placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default TodoList;
