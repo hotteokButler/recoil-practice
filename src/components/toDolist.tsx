a20e14a66a107d4b50f17e5f339149b93787969f
jisooyoon <ygs12567@gmail.com>
2022-04-12 17:43:19 +0900

    Use React Hool Form

--- src/components/toDolist.tsx
+++ src/components/toDolist.tsx
@@ -1,24 +1,51 @@
 import React, { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// const TodoList = () => {
+//   const [value, setValue] = useState("");
+//   const [error, setError] = useState("")
+//   // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//   //   const {
+//   //     currentTarget: { value },
+//   //   } = event;
+//   //   setValue(value);
+//   // };
+//   const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if(inputRef.current.value.length < 10) {
+//       return setError('To do should be logner than you inputed')
+//     } else {
+//       setError("")
+//     }
+//     setValue(inputRef.current.value);
+//     console.log(inputRef.current.value);
+//     inputRef.current.value = '';
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input type="text" ref={inputRef} placeholder="Write a to do" />
+//         <button>Add</button>
+//         {error !== "" ? <h1>{error}</h1>:null}
+//       </form>
+//     </div>
+//   );
+// };
 
 const TodoList = () => {
-  const [value, setValue] = useState<string>();
-  // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-  //   const {
-  //     currentTarget: { value },
-  //   } = event;
-  //   setValue(value);
-  // };
-  const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setValue(inputRef.current.value);
-    console.log(inputRef.current.value);
-    inputRef.current.value = '';
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input type="text" ref={inputRef} placeholder="Write a to do" />
+      <form>
+        <input {...register('Email')} placeholder="Eamil" />
+        <input {...register('Frist_Name')} placeholder="Frist Name" />
+        <input {...register('Last_Name')} placeholder="Last Name" />
+        <input {...register('Username')} placeholder="Username" />
+        <input {...register('Password')} placeholder="Password" />
+        <input {...register('Password1')} placeholder="Password1" />
         <button>Add</button>
       </form>
     </div>
